029d3c933184cdf76c751168d6cb389c4105b021
stellie <kims82@gmail.com>
2020-12-18 16:53:46 -0800

    register blogging post model and add test

--- mysite/blogging/models.py
+++ mysite/blogging/models.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User
 
 
 class Post(models.Model):
+    def __str__(self):
+        return self.title
+
     title = models.CharField(max_length=128)
     text = models.TextField(blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
